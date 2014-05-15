54b427f3c28ded5f1fa2e3a1d33bc072facc62df
Fabian Noth <noth@iai.uni-bonn.de>
2014-05-15 21:30:17 +0200

    updated demo data

--- DemoData/src/data/fsdb_serie.pl
+++ DemoData/src/data/fsdb_serie.pl
@@ -1,20 +1,20 @@
 :- dynamic id_handling:current_id/2.
 
-id_handling:current_id(fsdb_serie,7).
+id_handling:current_id(fsdb_serie,11).
 
 :- dynamic fsdb_serie_data/4.
 :- multifile fsdb_serie_data/4.
 
 fsdb_serie_data(1, 'Game of Thrones', 'USA', true).
-fsdb_serie_data(2, 'Stromberg', 'Deutschland', false).
-fsdb_serie_data(3, 'Lost', 'USA', true).
 fsdb_serie_data(4, 'Modern Family', 'USA', false).
+fsdb_serie_data(3, 'Lost', 'USA', true).
+fsdb_serie_data(2, 'Stromberg', 'Deutschland', false).
 
 :- dynamic fsdb_serie_stats/3.
 :- multifile fsdb_serie_stats/3.
 
 fsdb_serie_stats(1, 4, 40).
-fsdb_serie_stats(2, 5, 46).
-fsdb_serie_stats(3, 6, 121).
 fsdb_serie_stats(4, 5, 111).
+fsdb_serie_stats(3, 6, 124).
+fsdb_serie_stats(2, 5, 46).
 
